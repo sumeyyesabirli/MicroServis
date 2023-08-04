cda2025ba4f7411fbe75547335a18b7d715a06b2
Sumeyye <sumeyyesabirli@gmail.com>
2023-08-05 01:43:26 +0300

    Product controller

--- Microservices.Catalog/Services/ProductServices/ProductService.cs
+++ Microservices.Catalog/Services/ProductServices/ProductService.cs
@@ -32,7 +32,7 @@ namespace Microservices.Catalog.Services.ProductServices
 
         public  async Task<Response<NoContent>> DeleteProductAsync(string id)
         {
-            var value = await _productCollection.DeleteOneAsync(id);
+            var value = await _productCollection.DeleteOneAsync(x=>x.ProductID==id);
             if (value.DeletedCount > 0)
             {
                 return Response<NoContent>.Success(204);
